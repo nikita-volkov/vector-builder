b268229af352e9c73ca090c1f8cd85b89f5645cc
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-10-26 14:00:47 +0300

    Generalise Action

--- library/VectorBuilder/Private/Action.hs
+++ library/VectorBuilder/Private/Action.hs
@@ -2,12 +2,12 @@ module VectorBuilder.Private.Action
 where
 
 import VectorBuilder.Private.Prelude
-import qualified Data.Vector.Mutable as A
-import qualified Data.Vector as B
+import qualified Data.Vector.Generic.Mutable as A
+import qualified Data.Vector.Generic as B
 
 
 newtype Action element result =
-  Action (forall s. A.MVector s element -> ST s result)
+  Action (forall s vector. A.MVector vector element => vector s element -> ST s result)
   deriving (Functor)
 
 instance Applicative (Action element) where
@@ -20,6 +20,6 @@ unsafeWrite :: Int -> element -> Action element ()
 unsafeWrite index element =
   Action (\mVector -> A.unsafeWrite mVector index element)
 
-unsafeWriteMany :: Int -> B.Vector element -> Action element ()
+unsafeWriteMany :: B.Vector vector element => Int -> vector element -> Action element ()
 unsafeWriteMany startingIndex appendedVector =
   Action (\mVector -> B.ifoldM' (\_ index element -> A.unsafeWrite mVector (startingIndex + index) element) () appendedVector)
