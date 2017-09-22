94d647e85069ff7472bd3ff5aba30d10269e1001
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-09-22 20:00:18 +0300

    Boxed and unboxed benchmarks

--- foldl-benchmark/Main.hs
+++ foldl-benchmark/Main.hs
@@ -5,23 +5,45 @@ import Criterion
 import Criterion.Main
 import qualified Control.Foldl as A
 import qualified Data.Vector as F
+import qualified Data.Vector.Unboxed as G
 import qualified VectorBuilder.Builder as N
 import qualified VectorBuilder.Vector as O
 
 
 main =
-  defaultMain [group 1000, group 10000, group 100000, group 1000000, group 10000000, group 100000000]
+  defaultMain [boxed, unboxed]
   where
-    group size =
-      bgroup (show size)
-      [
-        bench "vector-builder" (nf foldWithBuilder input),
-        bench "default" (nf foldDefault input)
-      ]
+    sizes =
+      [1000, 100000, 10000000]
+    boxed =
+      bgroup "boxed" (map group sizes)
       where
-        input =
-          [0..size]
-        foldWithBuilder input =
-          A.fold (A.foldMap N.singleton O.build) input :: Vector Int
-        foldDefault input =
-          runST (A.foldM A.vector input) :: Vector Int
+        group size =
+          bgroup (show size)
+          [
+            bench "vector-builder" (nf foldWithBuilder input),
+            bench "default" (nf foldDefault input)
+          ]
+          where
+            input =
+              [0..size]
+            foldWithBuilder input =
+              A.fold (A.foldMap N.singleton O.build) input :: Vector Int
+            foldDefault input =
+              runST (A.foldM A.vector input) :: Vector Int
+    unboxed =
+      bgroup "unboxed" (map group sizes)
+      where
+        group size =
+          bgroup (show size)
+          [
+            bench "vector-builder" (nf foldWithBuilder input),
+            bench "default" (nf foldDefault input)
+          ]
+          where
+            input =
+              [0..size]
+            foldWithBuilder input =
+              A.fold (A.foldMap N.singleton O.build) input :: G.Vector Int
+            foldDefault input =
+              runST (A.foldM A.vector input) :: G.Vector Int
