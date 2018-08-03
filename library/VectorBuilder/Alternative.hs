0e86da5da476a3bb99500cf9d42a156f1afa3470
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-03 09:52:46 +0300

    Add "some"

--- library/VectorBuilder/Alternative.hs
+++ library/VectorBuilder/Alternative.hs
@@ -5,7 +5,7 @@ For instance, they can be applied with parsing libraries.
 module VectorBuilder.Alternative
 where
 
-import VectorBuilder.Prelude
+import VectorBuilder.Prelude hiding (many, some)
 import Data.Vector (Vector)
 import qualified VectorBuilder.Builder as A
 import qualified VectorBuilder.Vector as B
@@ -13,7 +13,18 @@ import qualified VectorBuilder.Vector as B
 
 {-# INLINABLE many #-}
 many :: Alternative m => m a -> m (Vector a)
-many m =
-  fmap B.build loop
-  where
-    loop = ((<>) <$> A.singleton <$> m <*> loop) <|> pure mempty
+many = fmap B.build . manyBuilder
+
+{-# INLINABLE manyBuilder #-}
+manyBuilder :: Alternative m => m a -> m (A.Builder a)
+manyBuilder m = let
+  loop = ((<>) <$> A.singleton <$> m <*> loop) <|> pure mempty
+  in loop
+
+{-# INLINABLE some #-}
+some :: Alternative m => m a -> m (Vector a)
+some m = B.build <$> someBuilder m
+
+{-# INLINABLE someBuilder #-}
+someBuilder :: Alternative m => m a -> m (A.Builder a)
+someBuilder m = (<>) <$> A.singleton <$> m <*> manyBuilder m
